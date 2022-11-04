1512eee2a5720e96a98589d93820caa5d36e3a97
4ndroid3 <andijaimo@gmail.com>
2022-11-04 11:37:51 -0300

    Backend NODE con express, sistema de autenticacion

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./db/config');
+require('dotenv').config(); // Al levantarse este import disponibiliza las variables de entorno en el archivo .env
+
+
+
+// crear el servidor de express.
+const app = express();
+
+// Base de datos
+dbConnection()
+
+// Directorio público
+app.use( express.static('public') );
+
+// CORS
+app.use( cors() );
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+app.use( '/api/auth', require('./routes/auth') );
+
+
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
